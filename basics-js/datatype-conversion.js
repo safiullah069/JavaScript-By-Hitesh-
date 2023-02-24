0c9b37a08297a05b815f526328c37360e5a0e7d0
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-02-24 21:23:37 +0000

    DataType Ki Tabdiliyan (Conversion)

---
+++ basics-js/datatype-conversion.js
@@ -0,0 +1,124 @@
+// DataType Conversion In Numbers
+
+// const age = "33";
+
+// console.log(age);
+// console.log(typeof(age));
+// console.log(typeof age);
+
+// const convertedAge = Number(age)
+// console.log(convertedAge);
+// console.log(typeof convertedAge);
+
+
+// const age2 = "33aa";
+
+// console.log(age2);
+// console.log(typeof(age2));
+// console.log(typeof age2);
+
+// const convertedAge2 = Number(age2)
+// console.log(convertedAge2);
+// console.log(typeof convertedAge2);
+
+
+// const LoggedIn = true;
+
+// console.log(LoggedIn);
+// console.log(typeof(LoggedIn));
+// console.log(typeof LoggedIn);
+
+// const convertedLoggedIn = Number(LoggedIn)
+// console.log(convertedLoggedIn);
+// console.log(typeof convertedLoggedIn);
+
+
+// const nullfied = null;
+
+// console.log(nullfied);
+// console.log(typeof(nullfied));
+// console.log(typeof nullfied);
+
+// const convertednullfied = Number(nullfied)
+// console.log(convertednullfied);
+// console.log(typeof convertednullfied);
+
+// "33" => 33(Number)
+// "33aa" => NaN
+// true => 1; false => 0
+// null => 0
+
+// DataType Conversion In Strings
+
+// let numbCheck = 33;
+
+// console.log(numbCheck);
+// console.log(typeof numbCheck);
+
+// let stringCheck = String(numbCheck);
+// console.log(stringCheck);
+// console.log(typeof stringCheck);
+
+
+// let boolCheck = true;
+
+// console.log(boolCheck);
+// console.log(typeof boolCheck);
+
+// let stringCheckBool = String(boolCheck);
+// console.log(stringCheckBool);
+// console.log(typeof stringCheckBool);
+
+// For Strings 
+
+// 33 => "33"
+// true => "true"
+
+// Datatype Conversion For Boolean
+
+// For String 
+
+// let stringCheck = "safi";
+
+// console.log(stringCheck);
+// console.log(typeof stringCheck);
+
+// let BoolcheckString = Boolean(stringCheck);
+// console.log(BoolcheckString);
+// console.log(typeof BoolcheckString);
+
+// For Empty String 
+
+// let stringCheckEmpty = "";
+
+// console.log(stringCheckEmpty);
+// console.log(typeof stringCheckEmpty);
+
+// let BoolstringCheckEmpty = Boolean(stringCheckEmpty);
+// console.log(BoolstringCheckEmpty);
+// console.log(typeof BoolstringCheckEmpty);
+
+
+// for Numbers 
+
+// let numCheck = 1
+
+// console.log(numCheck);
+// console.log(typeof numCheck);
+
+// let convertnumCheck = Boolean(numCheck)
+// console.log(convertnumCheck);
+// console.log(typeof convertnumCheck);
+
+// let numCheckfalse = -2
+
+// console.log(numCheckfalse);
+// console.log(typeof numCheckfalse);
+
+// let convertnumCheckFalse = Boolean(numCheckfalse)
+// console.log(convertnumCheckFalse);
+// console.log(typeof convertnumCheckFalse);
+
+// 0 => false
+// 1 => true
+// -1 => true 
