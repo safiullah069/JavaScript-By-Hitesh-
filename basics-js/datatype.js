584766338a7a56fd221ce7f2c6ae0c2b6c5c935d
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-02-24 20:16:21 +0000

    Data type Ki Pareshanin

---
+++ basics-js/datatype.js
@@ -0,0 +1,31 @@
+// Discussion On Datatypes
+
+"use strict";
+
+const name = "Safi Ullah"
+
+let age = 23;
+const loggendIn = true;
+
+let loyal;
+let test = undefined;
+
+let testNull = null;
+// You Cannot just defined the const you need to intialize it unlike let or var
+// const loyal2; 
+
+console.log(name);
+console.log(age);
+console.log(loggendIn);
+
+// Typeof testing of differnet variables
+
+console.log(typeof loyal);
+console.log(typeof test);
+console.log(typeof testNull);
+console.log(typeof name);
+console.log(typeof loggendIn);
+console.log(typeof age);
+
+// console.log(typeof loyal2)
+
