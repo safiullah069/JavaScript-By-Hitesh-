696d4c9493afb1260e169c574345e748eff162e5
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-02-26 20:38:30 +0000

    Confucion Clear

---
+++ basics-js/conversion-confusion.js
@@ -0,0 +1,55 @@
+const value = 2;
+const newValue = -value
+// console.log(newValue);
+
+const add = 2+2;
+const minus = 2-2;
+const multiply = 2*2;
+const divide = 2/2;
+const remainder = 2%2;
+const power = 3**2;
+
+
+// console.table([add, minus, multiply, divide, remainder, power])
+
+const firstName = "Safi"
+const lastName = "Ullah"
+const fullName = firstName + lastName;
+
+// console.log(fullName);
+
+const anotherTry = "Safi"
+const trySecond = `${anotherTry} Ullah`
+// console.log(trySecond);
+
+const demo1 = 1;
+const demo2 = 2;
+const demo3 = 1 + "2";
+const demo4 = "1" + 2;
+const demo5 = "1" + 2 + 2;
+const demo6 = "1" + 2 + "2";
+const demo7 = 1 + "2" + 2;
+const demo8 = 1 + 2 + "2";
+const demo9 = 1 + 2 + "2" + 3 + 4;
+
+
+// console.table([demo1, demo2, demo3, demo4, demo5, demo6, demo7, demo8, demo9])
+
+const incTest = 2
+
+let checkTest = 2;
+++checkTest
+checkTest--
+console.log(checkTest);
+
+const checkmate = -true
+const checkdate = +true
+const checklate = +"safi"
+const checkback = +""
+
+// console.log(checkmate);
+// console.log(checkdate);
+// console.log(checklate);
+// console.log(checkback);
+
+
