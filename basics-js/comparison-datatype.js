333a73fd3d116a476eeaee9eb328c98e487513ac
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-02-28 00:07:11 +0000

    Comparison Sirf Baradari Mein

---
+++ basics-js/comparison-datatype.js
@@ -0,0 +1,30 @@
+// Normal comparison operators
+
+// console.log(1 == 1);
+// console.log(1 < 1);
+// console.log(1 > 1);
+// console.log(1 <= 1);
+// console.log(1 >= 1);
+// console.log(1 != 1);
+
+// In Js =={Equality} And >, <, >=, <= {Comparison} Operators Operates Differently 
+
+// console.log("2" == 2);
+// console.log("2" !== 1);
+
+// console.log(null < 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// They Are Working Differently Because These Operators Operates Differently
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+
+// Because Undefined Is not converted Into Zero
+
+console.log("2" === 2);
+// Because === Checks Both The Value And The Type OF Datatype
