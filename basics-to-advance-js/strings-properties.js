c614fc2b8b0ad86ca31e25ba5d7aa53a44fe8d04
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-01 11:55:49 +0000

    string and properties

---
+++ basics-to-advance-js/strings-properties.js
@@ -0,0 +1,22 @@
+console.log(`Checking Files`);
+
+// < ---------- Constructor ---------- > 
+
+const a = new String("Hello world"); // a === "Hello world" is false
+const b = String("Hello world"); // b === "Hello world" is true
+a instanceof String; // is true
+b instanceof String; // is false
+typeof a; // "object"
+typeof b; // "string"
+
+/*
+When String is called as a constructor (with new), it creates a String object, which is not a primitive.
+
+When String is called as a function String(), it coerces the parameter to a string primitive.
+*/
+
+// < --------------- .length ------------ > 
+
+const str = `Safi Check`;
+
+console.log(str.length);
