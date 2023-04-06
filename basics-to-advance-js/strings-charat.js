b854e8fd857ad2ef80f5a3f411f147306d193ae7
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-06 22:30:24 +0000

    charAt()

--- basics-to-advance-js/strings-charat.js
+++ basics-to-advance-js/strings-charat.js
@@ -1,2 +1,19 @@
 console.log(`Testing Done`);
 
+// < ----------------- Strings.charAt() ------------------- >
+
+const Company = `Devsinc`
+console.log(Company.charAt(3));
+console.log(Company.charAt(23));
+console.log(Company.charAt(-3));
+console.log(Company.charAt());
+
+// An integer between 0 and str.length - 1. If the index cannot be converted to the integer or no index is provided, the default is 0, so the first character of str is returned.
+
+// If the index you supply is out of this range, JavaScript returns an empty string.
+
+// ----------------  How To Get The Last Character Of The String ---------
+
+console.log(Company.charAt(Company.length - 1));
+
+
