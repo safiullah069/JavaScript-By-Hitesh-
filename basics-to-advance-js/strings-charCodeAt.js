37ca737d4ba665d543f30d9d7fe7d6010199c0f2
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-06 22:48:56 +0000

    charCodeAt()

---
+++ basics-to-advance-js/strings-charCodeAt.js
@@ -0,0 +1,12 @@
+console.log(`Testing Done`);
+
+// A number representing the UTF-16 code unit value of the character at the given index. If index is out of range, charCodeAt() returns NaN.
+
+// An integer greater than or equal to 0 and less than the length of the string. If index is not a number, it defaults to 0.
+
+const myName = `Safiullah`
+console.log(myName.charCodeAt(0));
+console.log(myName.charAt(0));
+
+console.log("ABC".charCodeAt(0));
+// returns 65
