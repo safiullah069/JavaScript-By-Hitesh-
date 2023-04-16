96f931db467123a5ca20e496902087e0a6b1e0c0
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-16 16:50:13 +0000

    done

---
+++ basics-to-advance-js/strings-methods.js
@@ -0,0 +1,71 @@
+console.log(`Checking Files`);
+
+// < ----------------- String.at() -------------- >
+
+let str1 = `Laptop`;
+// console.log(str1.at(2));
+
+// < --------------- Sring.charAt() -------------- >
+
+let str2 = `Mobile`;
+
+// console.log(str2.charAt(4));
+
+// < --------------- String.charCodeAt() -------------- >
+
+let str3 = `Keyboard`;
+
+// console.log(str3.charCodeAt(1), str3.charAt(1));
+
+// < -------------- String.codePointAt() -------------- >
+
+let str4 = `😍`;
+
+// console.log(str4.codePointAt());
+// console.log(str4.codePointAt(0));
+// console.log(str4.codePointAt(1));
+
+
+// < --------------- String.concat() ----------------- >
+
+let str5 = `Safi`;
+let str6 = `Ullah`;
+
+// console.log(str5.concat(` ${str6}`));
+
+// < -------------- String.endsWith() ---------------- >
+
+let str7 = `Is This Entertaining, My Patience Is Vaining, I'm Invincible`;
+
+// console.log(str7.endsWith(`e`));
+// console.log(str7.endsWith(`Invincible`));
+
+// < ------------- String.fromCharCode -------------- >
+
+
+// console.log(String.fromCharCode(101));
+
+// < ------------- String.fromCodePoint() ------------- >
+
+// console.log(String.fromCodePoint(128525));
+
+// < -------------- String.includes() --------------- >
+
+let str8 = `Pain! You Made Me A Believer`;
+
+// console.log(str8.includes(`!`));
+// console.log(str8.includes(`Pain!`));
+// console.log(str8.includes(`Believer`));
+
+// < ------------ String.indexOf() -------------- >
+
+let str9 = `Why Why?`;
+check = str9.toLowerCase();
+
+// console.log(check);
+// console.log(check.indexOf(`why`));
+
+// < ----------- String.isWellFormed() ----------- >
+
+
+
