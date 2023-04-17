d6620af52e01513ed436bdf103a09cc3ecfd8691
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-17 17:14:39 +0000

    done

---
+++ basics-to-advance-js/numbers.js
@@ -0,0 +1,22 @@
+console.log(`Checking Files`);
+
+const age = 23;
+
+
+// console.log(age);
+// console.log(typeof age);
+
+const score = new Number(23);
+const balance = Number(12);
+
+// console.log(balance);
+// console.log(typeof balance);
+
+
+// console.log(score);
+// console.log(typeof score);
+// console.log(score.valueOf());
+
+// console.log(score == age); // true
+// console.log(score === age); // false
+
