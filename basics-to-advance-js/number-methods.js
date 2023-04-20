85cc8991d473ed3e17d57660499b71303646cea7
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-20 00:55:31 +0000

    done1

--- basics-to-advance-js/number-methods.js
+++ basics-to-advance-js/number-methods.js
@@ -1,1 +1,33 @@
 console.log(`Checking File`);
+
+console.log(`Checking File`);
+// < -------------- Numbers.toString() --------------- >
+const str1 = `125`
+// console.log(str1);
+// console.log(typeof str1);
+
+let numCheck = Number(str1);
+// console.log(numCheck);
+// console.log(typeof numCheck);
+
+let numToStr = 12354;
+// console.log(numToStr);
+// console.log(typeof(numToStr.toString()));
+// console.log(typeof numToStr)
+
+const num1 = 8;
+// console.log(num1.toString(16));
+
+// < ------------------- Number.toExponential() ----------------- >
+
+const str2 = 564;
+// console.log(str2.toExponential(2));
+
+const str3 = 65.678798;
+// console.log(str3.toExponential());
+// console.log(str3.toExponential(2));
+// console.log(str3.toExponential(3));
+// console.log(str3.toExponential(4));
+// console.log(str3.toExponential(5));
+
+// < ------------- Number.toFixed() ---------------- >
