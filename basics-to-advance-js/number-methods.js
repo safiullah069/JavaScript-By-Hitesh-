d7a1a3904904b975e5cd853ceea67b9f959ff150
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-04-26 09:13:31 +0000

    addedd

--- basics-to-advance-js/number-methods.js
+++ basics-to-advance-js/number-methods.js
@@ -57,4 +57,51 @@ const numStr3 = 0.0042;
 
 const numStr2 = 123;
 
-console.log(numStr2.toLocaleString(`ar-EG`));
+// console.log(numStr2.toLocaleString(`ar-EG`));
+
+// < ------------- Number.parseInt() ---------------- >
+
+// console.log(Number.parseInt(124.76));
+
+// < ------------- Number.parseFloat() ---------------- >
+
+// console.log(Number.parseFloat(123.321));
+
+// < --------------- Number.isSafeInteger() -------------- >
+
+// console.log(Number.isSafeInteger(123));
+// console.log(Number.isSafeInteger(123 ** 321));
+
+// < ------------- Number.isNan() ---------------- >
+
+// console.log(Number.isNaN(Number(746283)));
+// console.log(Number.isNaN(`hf`));
+// console.log(Number.isNaN("NaN"));
+// console.log(isNaN("Blabala"));
+// console.log(isNaN("8763247"));
+
+// < ------------ Number.isInteger() ----------------- >
+
+// console.log(Number.isInteger(132));
+// console.log(Number.isInteger(`fsdsdf`));
+// console.log(Number.isInteger(3123.142));
+
+function fit(x, y) {
+    if (Number.isInteger(x / y)) {
+        return `Fits`
+    }else{
+        return `Not Fitted`
+    }
+}
+
+// console.log(fit(8, 4));
+// console.log(fit(2, 4));
+
+// < -------------- Number.isFinite() ------------- >
+
+// console.log(Number.isFinite(123));
+// console.log(Number.isFinite(2/2));
+// console.log(Number.isFinite(2/0));
+// console.log(Number.isFinite(0/0));
+// console.log(Number.isFinite(Infinity));
+
--- basics-to-advance-js/number-properties.js
+++ basics-to-advance-js/number-properties.js
@@ -1,1 +1,2 @@
 console.log(`Checking Files`);
+
