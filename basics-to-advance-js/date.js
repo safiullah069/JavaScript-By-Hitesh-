c07a531a64be57f96730f516464830c1449654fa
safiullah069 <54856342+safiullah069@users.noreply.github.com>
2023-05-12 23:37:56 +0000

    dededede

--- basics-to-advance-js/date.js
+++ basics-to-advance-js/date.js
@@ -3,14 +3,28 @@ console.log(`Checking Files`);
 const birthday = new Date('August 19, 1975 23:15:30');
 const date1 = birthday.getDate();
 
-console.log(date1);
+// console.log(date1);
 // Expected output: 19
 
 const eventBirthday = new Date(`2002-4-23`);
 
-console.log(eventBirthday.getDate());
+// console.log(eventBirthday.getDate());
 const day = eventBirthday.getDay();
 const dayArray = [`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`, ]
-console.log(dayArray[day]);
+// console.log(dayArray[day]);
 
+const today = new Date();
+// console.log(today);
+
+const customizeDate = new Date(`2000-1-22`)
+console.log(customizeDate);
+
+const calculationOnDate = () => {const birthday = customizeDate.getDay();
+    const birthdayMonth = customizeDate.getMonth();
+    arrayDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "THursday", "Friday", "Saturday"];
+    arrayMonth = ["January", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+    return `I was born on ${customizeDate.getDate()} ${arrayMonth[birthdayMonth]} And The Day Was ${arrayDay[birthday]} And The Year Was ${customizeDate.getFullYear()}`
+}
+
+console.log(calculationOnDate());
 
